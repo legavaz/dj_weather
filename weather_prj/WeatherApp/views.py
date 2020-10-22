d2e4b8cad4ae58f64723dcf829c8a5b2f371a533
dvi <legavaz@gmail.com>
2020-10-22 16:43:58 +0300

    +

--- weather_prj/WeatherApp/views.py
+++ weather_prj/WeatherApp/views.py
@@ -13,8 +13,8 @@ def index(request):
 
     if request.method == "POST":
         form = CityForm(request.POST)
-        print('request',request.POST)
-        print('class',form)
+        # print('request',request.POST)
+        # print('class',form)
         form.save()
 
     # Очистим форму    
